758ce5c995bd5d4ae899f0e8cf9250611846f667
BDG <bdg@localhost.localdomain>
2017-12-13 19:10:17 +0530

    updated microservice code 13-12-2017

--- SpringDataMongoDB/src/main/java/com/springboot/datamongodb/controller/Dashboardcontroller.java
+++ SpringDataMongoDB/src/main/java/com/springboot/datamongodb/controller/Dashboardcontroller.java
@@ -4,7 +4,10 @@ package com.springboot.datamongodb.controller;
 import java.net.URI;
 import java.util.List;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.TypedAggregation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,38 +16,56 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 import com.springboot.datamongodb.model.Dashboard;
+//import com.springboot.datamongodb.model.distinctDashboard;
 import com.springboot.datamongodb.repository.DashboardMongoRepository;
-
+import com.mongodb.client.AggregateIterable;
 /*import com.in28minutes.springboot.model.Course;
 import com.in28minutes.springboot.model.Dashboard;
 import com.in28minutes.springboot.service.DashboardService;
 import com.in28minutes.springboot.service.StudentService;;*/
+import com.springboot.datamongodb.SpringDataMongoDbApplication;
 
 @RestController
 @RequestMapping("/Dashboard")
 
 public class Dashboardcontroller {
-	private DashboardMongoRepository Dashboardrepo;
+	//private DashboardMongoRepository Dashboardrepo;
+	SpringDataMongoDbApplication objdashboard = new SpringDataMongoDbApplication();
 	
-	public Dashboardcontroller(DashboardMongoRepository Dashboardrepo){
-		this.Dashboardrepo = Dashboardrepo;
+	
+	@GetMapping("/browser")
+	public AggregateIterable<Document>  getBrowser() {
+	AggregateIterable<Document> objbrowser = objdashboard.getBrowser();
+	return objbrowser;
 	}
 	
-	@GetMapping("/page1")
-	public List<Dashboard> getAll(){
-	List<Dashboard> dashboard = this.Dashboardrepo.findAll();
-	return dashboard;
 	
-	}
+	@GetMapping("/country")
+	public AggregateIterable<Document>  getCountry() {
+		AggregateIterable<Document> objcountry = objdashboard.getCountry();
+		return objcountry;
+		}
 	
-	@GetMapping("/{id}")
-	public List<Dashboard> getById(@PathVariable("id") String id){
-	List<Dashboard> dashboard = this.Dashboardrepo.findById(id);
-	return dashboard;
+	@GetMapping("/language")
+	public AggregateIterable<Document>  getLanguage() {
+		AggregateIterable<Document> objlanguage = objdashboard.getLanguage();
+		return objlanguage;
+		}
 	
-	}
+	@GetMapping("/device")
+	public AggregateIterable<Document>  getDevice() {
+		AggregateIterable<Document> objdevice = objdashboard.getDevice();
+		return objdevice;
+		}
+	
+	@GetMapping("/os")
+	public AggregateIterable<Document>  getOs() {
+		AggregateIterable<Document> objos = objdashboard.getOs();
+		return objos;
+		}
+	
 	
 	/*@GetMapping("/browser/{id}")
 	public List<Dashboard> getByIdBetween(@PathVariable("id") String id){
--- SpringDataMongoDB/src/main/java/com/springboot/datamongodb/model/distinctDashboard.java
+++ SpringDataMongoDB/src/main/java/com/springboot/datamongodb/model/distinctDashboard.java
@@ -1,48 +1,27 @@
-package com.springboot.datamongodb;
+/*package com.springboot.datamongodb.model;
 
 import java.util.Arrays;
+
 import org.bson.Document;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.mongodb.AggregationOutput;
-import com.mongodb.BasicDBObject;
-import com.mongodb.BasicDBObjectBuilder;
-import com.mongodb.Block;
-import com.mongodb.DBObject;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-//import com.springboot.datamongodb.model.distinctDashboard;
 
-
-@SpringBootApplication
-public class SpringDataMongoDbApplication {
+public class distinctDashboard {
 	
-	/*@Autowired
-	DashboardMongoRepository DashboardRepo;*/
 	MongoClient mongoClient = new MongoClient();
     MongoDatabase database = mongoClient.getDatabase("test");
     MongoCollection<Document> collection = database.getCollection("dashboard");
 	
-	
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataMongoDbApplication.class, args);
-		/*distinctDashboard objdashboard = new distinctDashboard();
-		AggregateIterable<Document> objbrowser = 
-		objdashboard.getBrowser();
-		objdashboard.getCountry();
-		objdashboard.getLanguage();
-		objdashboard.getDevice();*/
-}
-
-public AggregateIterable<Document>  getBrowser() {
+	public AggregateIterable<Document>  getBrowser() {
 		
 		AggregateIterable<Document> dbrowser = collection.aggregate(Arrays.asList(
-					new Document("$project", new Document("_id", 0)
+					new Document("$project", new Document("_id", 1)
 	    	                    .append("browser", "$Dashboard.browser")),
 	    	                   // .append("date", "$views.date"))
-	    	        //new Document("$match", new Document("_id", "2017-02-22")),
+	    	        new Document("$match", new Document("_id", "2017-02-22")),
                     new Document("$group", new Document("_id","distinctBrowser")
                                .append("browsers",new Document("$addToSet","$browser")))));
 		
@@ -58,10 +37,10 @@ public AggregateIterable<Document>  getBrowser() {
 public AggregateIterable<Document>  getCountry() {
 		
 		AggregateIterable<Document> dcountry = collection.aggregate(Arrays.asList(
-	    	        new Document("$project", new Document("_id", 0)
+	    	        new Document("$project", new Document("_id", 1)
 	    	                    .append("country", "$Dashboard.country")),
 	    	                   // .append("date", "$views.date"))
-	    	        //new Document("$match", new Document("_id", "2017-02-22")),
+	    	        new Document("$match", new Document("_id", "2017-02-22")),
                     new Document("$group", new Document("_id","distinctCountry")
                                .append("country",new Document("$addToSet","$country")))));
 		
@@ -76,10 +55,10 @@ public AggregateIterable<Document>  getCountry() {
 public AggregateIterable<Document>  getLanguage() {
 	
 	AggregateIterable<Document> dlanguage = collection.aggregate(Arrays.asList(
-    	       new Document("$project", new Document("_id", 0)
+    	       new Document("$project", new Document("_id", 1)
     	                    .append("language", "$Dashboard.language")),
     	                   // .append("date", "$views.date"))
-    	        //new Document("$match", new Document("_id", "2017-02-22")),
+    	        new Document("$match", new Document("_id", "2017-02-22")),
                 new Document("$group", new Document("_id","distinctLanguage")
                            .append("language",new Document("$addToSet","$language")))));
 
@@ -93,10 +72,10 @@ return dlanguage;
 public AggregateIterable<Document>  getDevice() {
 	
 	AggregateIterable<Document> ddevice = collection.aggregate(Arrays.asList(
-    	        new Document("$project", new Document("_id", 0)
+    	        new Document("$project", new Document("_id", 1)
     	                    .append("device", "$Dashboard.device")),
     	                   // .append("date", "$views.date"))
-    	        //new Document("$match", new Document("_id", "2017-02-22")),
+    	        new Document("$match", new Document("_id", "2017-02-22")),
                 new Document("$group", new Document("_id","distinctDevice")
                            .append("device",new Document("$addToSet","$device")))));
 
@@ -107,21 +86,5 @@ public AggregateIterable<Document>  getDevice() {
 return ddevice;
 }
 	
-public AggregateIterable<Document>  getOs() {
-	
-	AggregateIterable<Document> dos = collection.aggregate(Arrays.asList(
-    	        new Document("$project", new Document("_id", 0)
-    	                    .append("os", "$Dashboard.os")),
-    	                   // .append("date", "$views.date"))
-    	        //new Document("$match", new Document("_id", "2017-02-22")),
-                new Document("$group", new Document("_id","distinctOs")
-                           .append("device",new Document("$addToSet","$os")))));
-
-	 for (Document dbObject : dos)
-     {
-         System.out.println(dbObject);
-     }
-return dos;
-}	
-	
 }
+*/
